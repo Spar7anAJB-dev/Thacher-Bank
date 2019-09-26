a51ecd718022a777065f2295f2a611e15bf272aa
Spar7anAJB-dev <55842793+Spar7anAJB-dev@users.noreply.github.com>
2019-09-26 10:23:59 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var person = alert("This is not a real bank");
+
+
+
+
+function Bank()
+{
+	window.location.href='bank.html'
+}
+
+function Login()
+{
+	window.location.href='login.html'
+}
---
+++ signin.js
@@ -0,0 +1,10 @@
+var pass = "psd"
+
+if (pass == "Aidyn") {
+
+  var i = false;
+
+} 
+if (i == false) {
+ window.location = 'index.html';
+} 
